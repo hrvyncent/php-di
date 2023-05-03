624aafd151675f72a3ce268eb5e880cd1b48ecff
harvicapino22 <harvicapino22@gmail.com>
2023-05-03 08:03:01 +0800

    created Container

--- src/Container.php
+++ src/Container.php
@@ -1,38 +1,45 @@
 <?php
+declare(strict_types=1);
 
 namespace Xeno;
 
 use Psr\Container\ContainerInterface;
+use ReflectionClass;
+use ReflectionIntersectionType;
+use ReflectionNamedType;
+use ReflectionParameter;
+use ReflectionUnionType;
 use Xeno\Exceptions\BindingsNotFoundException;
 use Xeno\Exceptions\ContainerException;
 
-class Container implements ContainerInterface
+final class Container implements ContainerInterface
 {
     /**
-     * The container bindings.
+     * The array containing all the application class bindings.
      */
-    private array $bindings = [];
+    private array $bindings = [];    
 
     /**
-     * Get the concrete implementation of the given abstract.
+     * Get the entry in the bindings.
      * 
      * @throws \Xeno\Exceptions\BindingsNotFoundException
      * @throws \Xeno\Exceptions\ContainerException
      */
     public function get(string $abstract) : mixed
     {
-        if (! $this->has($abstract)) {
+        if (!$this->has($abstract)) {
             throw new BindingsNotFoundException(
-                message: sprintf('The [%s] does the found in the container bindings.', $abstract)
+                sprintf("The [%s] do not exists in the bindings.", $abstract)
             );
         }
 
         try {
-            $concrete = $this->resolve($abstract);
+            $binding = $this->bindings[$abstract];            
+            $concrete = $this->make($binding);
         } catch (ContainerException $ex) {
-            throw new ContainerException(
-                message: sprintf('The concrete implementation of [%s] is cannot be resolve.', $abstract),
-            );
+            
+            //Throw the exception if there is one
+            throw $ex;
         }
 
         return $concrete;
@@ -43,13 +50,54 @@ class Container implements ContainerInterface
      */
     public function has(string $abstract): bool
     {
-        return (bool) isset($this->bindings[$abstract]);
+        return (bool) isset ($this->bindings[$abstract]);
     }
 
     /**
-     * Bind the concrete to the abstract.
+     * Resolve the concrete implementation of the given abstract.
+     * 
+     * @throws \Micra\Core\Exceptions\ContainerException
      */
-    public function bind(string $abstract, mixed $concrete = null) : void
+    public function make(string $concrete) : mixed
+    {
+        // Get the concrete class information using Reflection Class
+        $reflection = $this->getReflection($concrete);
+
+        /**
+         * If the concrete class is not instantiable, throw an exception.
+         */
+        if (! $reflection->isInstantiable()) {
+            throw new ContainerException(
+                sprintf("The [%s] cannot be resolve because its not instantiable", $reflection->getName())
+            );
+        }
+
+        $constructor = $reflection->getConstructor();
+
+        /**
+         * Return the instance of the concrete if there's no constructor found.
+         */
+        if (is_null($constructor)) {
+            return $reflection->newInstance();
+        }
+
+        /**
+         * If the number of parameters is equals to 0 we will
+         * return its instance instead of continuing.
+        */
+        if ($constructor->getNumberOfParameters() === 0) {
+            return $reflection->newInstance();
+        }
+
+        $dependencies = $this->getDependencies($constructor->getParameters());
+
+        return $reflection->newInstanceArgs($dependencies);
+    }
+
+    /**
+     * Bind the concrete implementation to abstract
+     */
+    public function bind(string $abstract, string $concrete = null) : void
     {
         if (is_null($concrete)) {
             $concrete = $abstract;
@@ -58,6 +106,48 @@ class Container implements ContainerInterface
         $this->bindings[$abstract] = $concrete;
     }
 
-    public function resolve()
-    {}
+    /**
+     * Get the class information using `ReflectionClass`
+     */
+    private function getReflection(string $concrete) : ReflectionClass
+    {
+        return new ReflectionClass($concrete);
+    }
+
+    /**
+     * Get the resolve dependencies for the concrete class.
+     */
+    private function getDependencies(array $parameters) : array
+    {
+        return array_map(
+            callback: function (ReflectionParameter $param) {
+                $name = $param->getName();
+                $type = $param->getType();
+
+                if (is_null($type)) {
+                    throw new ContainerException(
+                        sprintf("The [%s] does not have a type hint so it can't be resolve", $name)
+                    );
+                }
+
+                if ($type instanceof ReflectionUnionType || $type instanceof ReflectionIntersectionType) {
+                    throw new ContainerException(
+                        sprintf(
+                            "The [%s] is failed to resolve. The [%s] cannot be an instance of %s or %s", 
+                            $name, $name, ReflectionUnionType::class,ReflectionIntersectionType::class
+                        )
+                    );
+                }
+
+                if (! $type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                    throw new ContainerException(
+                        sprintf("The [%s] is failed to resolve. The [%s] cannot be a built-in type.", $name)
+                    );
+                }
+
+                return $this->get($type->getName());
+            },
+            array: $parameters,
+        );
+    }
 }
--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -1,43 +1,95 @@
 <?php
 
-namespace Tests;
+namespace Tests\Core;
 
 use PHPUnit\Framework\TestCase;
+use Xeno\Exceptions\BindingsNotFoundException;
+use Xeno\Exceptions\ContainerException;
 use Xeno\Container;
 
-class ContainerTest extends TestCase
+final class ContainerTest extends TestCase
 {
-    private Container $container;
-    
+    private Container $app;
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->container = new Container;
+        $this->app = new Container;
     }
 
-    public function test_it_will_return_false_when_the_id_does_not_found()
+    public function test_it_will_return_false_if_the_bindings_do_not_exists()
     {
-        $result = $this->container->has(Dummy::class);
-
+        $result = $this->app->has(Dummy::class);
+    
         $this->assertFalse($result);
     }
 
-    public function test_it_will_return_true_if_the_id_exists_in_the_bindings()
+    public function test_it_will_return_the_concrete_name_if_the_abstract_exsist_it_the_bindings()
     {
-        $this->container->bind(Sample::class);
+        $this->app->bind(DummyInterface::class, Dummy::class);
+        $this->app->bind(DummyDependencyInterface::class, DummyDependency::class);
 
-        $result  = $this->container->has(Sample::class);
+        $result = $this->app->get(DummyInterface::class);
 
-        $this->assertTrue($result);
+        $this->assertInstanceOf(Dummy::class, $result);
     }
 
-    public function test_it_will_bind_the_concrete_implementation_in_the_container()
+    public function test_it_will_throw_a_binding_not_found_exception_if_the_bindings_not_found()
     {
-        $this->container->bind(Abstracts::class, Concrete::class);
+        $this->expectException(BindingsNotFoundException::class);
 
-        $result = $this->container->has(Abstracts::class);
-        
-        $this->assertTrue($result);
+        $this->app->get(DummyInterface::class);
     }
-}   
+
+    public function test_it_will_throw_an_exception_if_union_type()
+    {
+        $this->expectException(ContainerException::class);
+
+        $this->app->bind(DummyInterface::class, DummyWithUnionType::class);
+
+        $this->app->get(DummyInterface::class);
+    }
+
+    public function test_it_will_throw_an_exception_if_builtin_type()
+    {
+        $this->expectException(ContainerException::class);
+
+        $this->app->bind(DummyInterface::class, DummyWithBuiltInType::class);
+
+        $this->app->get(DummyInterface::class);
+    }
+
+    public function test_it_will_throw_an_exception_if_intersection_type()
+    {
+        $this->expectException(ContainerException::class);
+
+        $this->app->bind(DummyInterface::class, DummyWithIntersectionType::class);
+
+        $this->app->get(DummyInterface::class);
+    }
+}
+
+interface DummyInterface {}
+interface DummyDependencyInterface {}
+
+class DummyDependency implements DummyDependencyInterface {}
+class Dummy implements DummyInterface
+{
+    public function __construct(private DummyDependencyInterface $class) {}
+}
+
+class DummyWithUnionType implements DummyInterface
+{
+    public function __construct(private DummyInterface|DummyDependencyInterface $class) {}
+}
+
+class DummyWithBuiltInType implements DummyInterface
+{
+    public function __construct(private DummyInterface|DummyDependencyInterface $class) {}
+}
+
+class DummyWithIntersectionType implements DummyInterface
+{
+    public function __construct(private DummyInterface&DummyDependencyInterface $class) {}
+}
